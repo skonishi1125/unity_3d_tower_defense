881d56a99c91d4610313b7167d98aaa538011fb5
skonishi <skonishi1125@gmail.com>
2026-01-18 23:21:48 +0900

    クリックした場所にタワーが配置されるようにした

---
+++ Assets/Script/BuildController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildController : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] private GameObject towerPrefab;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Build();
+
+    }
+
+    private void Build()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayerMask))
+        {
+            Vector3 p = hit.point;
+            float s = .2f;
+
+            // 十字マーカー（赤）
+            Debug.DrawLine(p - Vector3.right * s, p + Vector3.right * s, Color.red, 10f);
+            Debug.DrawLine(p - Vector3.forward * s, p + Vector3.forward * s, Color.red, 10f);
+            Debug.DrawLine(p - Vector3.up * s, p + Vector3.up * s, Color.red, 10f);
+
+            // Ray 自体も可視化
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 10f);
+
+            Instantiate(towerPrefab, hit.point, Quaternion.identity);
+        }
+    }
+
+}
---
+++ Assets/Script/BuildMode.cs
@@ -0,0 +1,6 @@
+﻿public enum BuildMode
+{
+    None = 0,
+    Place = 1,
+    Demolish = 2,
+}
