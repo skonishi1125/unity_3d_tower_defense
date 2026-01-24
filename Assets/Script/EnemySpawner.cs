0f984b0795c64fa8bae7df34b3722170249ce76a
skonishi <skonishi1125@gmail.com>
2026-01-24 22:55:22 +0900

    コミット漏れ対応

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -28,30 +28,26 @@ public class EnemySpawner : MonoBehaviour
 
     private void Spawn(Transform spawnPoint)
     {
-
         // Instantiate(SpawnEnemies[0], spawnPoint);
         // ↑この形で呼び出すと、spawnPointのGameObjectの子要素として作られてしまう
         // 生成場所を指定したい場合は、以下のようにposition, rotationどちらも明示的に指定する必要がある
         // ちなみに第4引数にTransform parent などとすると、その親の子要素として位置も指定しつつ生成ができる
-        //var go = Instantiate(SpawnEnemies[0], spawnPoint.position, spawnPoint.rotation);
+        var go = Instantiate(SpawnEnemies[0], spawnPoint.position, spawnPoint.rotation);
 
-        Vector3 spawnPos = spawnPoint.position;
-        // 敵のスポーンy座標 = (scale * .25f) + (床の厚み * .5f)
-        //spawnPos.y = (SpawnEnemies[0].transform.localScale.y * .25f) + (RoadGenerator.RoadThickness * .5f);
-        //Debug.Log(spawnPos.y);
-        var go = Instantiate(SpawnEnemies[0], spawnPos, spawnPoint.rotation);
+        // 敵のスポーンy座標 = (Colliderの高さの半分) + (床の厚み * .5f)
         float spawnHeight = 0f;
         var col = go.GetComponent<Collider>();
         if (col != null)
             spawnHeight = col.bounds.extents.y;
 
-        Debug.Log(spawnHeight);
         spawnHeight += RoadGenerator.RoadThickness * .5f;
         go.transform.position = spawnPoint.position + Vector3.up * spawnHeight;
 
+        // 移動処理起動
         var mover = go.GetComponent<EnemyMovement>();
         mover.Initialize(waypoint);
 
+        // タイマー設定
         timer = duration;
         spawnCount++;
     }
