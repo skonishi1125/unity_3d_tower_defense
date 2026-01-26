aab9ee1b81681db14fc97cfa42b173240e6aab1d
skonishi <skonishi1125@gmail.com>
2026-01-26 22:40:52 +0900

    ダメージvfx書き換え処理の調整

--- Assets/Script/Enemy/EnemyVfx.cs
+++ Assets/Script/Enemy/EnemyVfx.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyVfx : MonoBehaviour
 {
-    private MeshRenderer mr;
+    private MeshRenderer[] mrs;
+    // MeshRendererをkeyにした、Rendererのマテリアル配列
+    // originalMaterials[mr] で、元マテリアルを配列で出せるような設計。
+    private Dictionary<MeshRenderer, Material[]> originalMaterials;
 
     [Header("Damage Vfx")]
     [SerializeField] private Material onDamageMaterial;
     [SerializeField] private float onDamageVfxDuration = .1f;
-    private Material originalMaterial;
+
     private Coroutine onDamageVfxCo;
 
     protected virtual void Awake()
     {
-        mr = GetComponentInChildren<MeshRenderer>();
-        originalMaterial = mr.material;
+        mrs = GetComponentsInChildren<MeshRenderer>();
+        originalMaterials = new Dictionary<MeshRenderer, Material[]>(mrs.Length); // lengthを指定することでメモリの節約になる
+
+        foreach (var mr in mrs)
+        {
+            // 1つのMeshRendererごとに、マテリアルを格納
+            // mrに複数マテリアルが付くケースがあるので（SubMeshと呼ぶ）、クローンして元配列自体を格納
+            // クローン結果はobjectで返るので、Material[]でキャストして格納
+            originalMaterials[mr] = (Material[])mr.materials.Clone();
+        }
     }
 
     public void PlayOnDamageVfx()
@@ -28,9 +40,31 @@ public class EnemyVfx : MonoBehaviour
 
     private IEnumerator OnDamageVfx()
     {
-        mr.material = onDamageMaterial;
+        // ダメージ
+        foreach (var mr in mrs)
+        {
+            if (mr == null) continue;
+
+            var materials = mr.materials;
+            for (int i = 0; i < materials.Length; i++)
+                materials[i] = onDamageMaterial;
+
+            mr.materials = materials;
+        }
+
         yield return new WaitForSeconds(onDamageVfxDuration);
-        mr.material = originalMaterial;
+
+        // 元マテリアルに戻す
+        foreach (var mr in mrs)
+        {
+            if (mr == null) continue;
+
+            if (originalMaterials.TryGetValue(mr, out var original))
+                mr.materials = original;
+        }
+
+        // もうこのCoroutineは走っていないという状態にしておく
+        onDamageVfxCo = null;
     }
 
 
