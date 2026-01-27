5de778549175ea99ddf4a7fb1e6e0f8a7643aa1e
skonishi <skonishi1125@gmail.com>
2026-01-27 23:12:36 +0900

    ビルボードスクリプトを用意して、UIがこちらを向くようにした

---
+++ Assets/Script/BillBoard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    [SerializeField] Camera targetCamera;
+
+    private void Awake()
+    {
+        if (!targetCamera)
+            targetCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        if (!targetCamera)
+            return;
+
+        // カメラと同じ向きに合わせて、体力バーをこちらに常に向ける
+        transform.rotation = targetCamera.transform.rotation;
+    }
+
+
+}
