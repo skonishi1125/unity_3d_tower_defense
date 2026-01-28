4d1d52bccf3e43683f2518cb4af358ca35f54265
skonishi <skonishi1125@gmail.com>
2026-01-28 19:47:19 +0900

    画面に所持金が表示されるようになった

---
+++ Assets/Script/UI/Hud.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Hud : MonoBehaviour
+{
+    [SerializeField] private EconomyManager em;
+    [SerializeField] private TextMeshProUGUI moneyAmount;
+
+    private void Awake()
+    {
+        if (em == null)
+            em = FindFirstObjectByType<EconomyManager>();
+
+        // 初期表示（購読前にAwake通知が終わっている可能性があるため）
+        UpdateMoneyAmount(em.CurrentMoney);
+    }
+
+    private void UpdateMoneyAmount(float currentMoney)
+    {
+        moneyAmount.text = $"{currentMoney} 円";
+    }
+
+    private void OnEnable()
+    {
+        if (em != null)
+            em.MoneyChanged += UpdateMoneyAmount;
+
+        // 保険で、有効化のたびに表示を正しくしておく
+        if (em != null)
+            UpdateMoneyAmount(em.CurrentMoney);
+    }
+
+    private void OnDisable()
+    {
+        if (em != null)
+            em.MoneyChanged -= UpdateMoneyAmount;
+    }
+
+
+}
