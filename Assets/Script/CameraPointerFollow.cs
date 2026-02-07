f741a61dba0b3180c0a2a5b60e0ca0b0f2da8283
skonishi <skonishi1125@gmail.com>
2026-02-07 22:40:27 +0900

    通常 , editモード別にカメラを追加

---
+++ Assets/Script/CameraPointerFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraPointerFollow : MonoBehaviour
+{
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private float moveRange = 5f;
+
+    private void Awake()
+    {
+        if (gameInput == null)
+        {
+            Debug.Log("CameraPointerFollow: gameInputを自動割当します");
+            gameInput = FindFirstObjectByType<GameInput>();
+        }
+    }
+
+    private void Update()
+    {
+        // マウスの画面座標（0〜1）を取得
+        Vector2 screenPos = new Vector2(
+            (gameInput.PointerPosition.x / Screen.width) * 2 - 1,
+            (gameInput.PointerPosition.y / Screen.height) * 2 - 1
+        );
+
+        // 中心を (0, 0) に変換 (-0.5 〜 0.5)
+        Vector2 offset = screenPos - new Vector2(0.5f, 0.5f);
+
+        transform.localPosition = new Vector3(screenPos.x * moveRange, 0, screenPos.y * moveRange);
+    }
+
+
+}
---
+++ Assets/Script/Manager/CameraManager.cs
@@ -0,0 +1,57 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // カメラ優先度
+    private const int PriorityActive = 20;
+    private const int PriorityInactive = 10;
+
+    [SerializeField] private StateManager stateManager;
+
+    [SerializeField] private CinemachineCamera overviewVCam;
+    [SerializeField] private CinemachineCamera buildVCam;
+
+    private void Awake()
+    {
+        if (stateManager == null)
+        {
+            Debug.Log("CameraManager: stateManagerを自動割当します");
+            stateManager = FindFirstObjectByType<StateManager>();
+        }
+
+        // 開始時のカメラ設定は、StateManager側でAwake時にInvokeされるので不要。
+        RefreshCameraPriority();
+    }
+
+    private void OnEnable()
+    {
+        if (stateManager != null)
+            stateManager.StateChanged += RefreshCameraPriority;
+    }
+
+    private void OnDisable()
+    {
+        if (stateManager != null)
+            stateManager.StateChanged -= RefreshCameraPriority;
+    }
+
+    private void RefreshCameraPriority()
+    {
+        if (stateManager.State == GameState.Edit)
+        {
+            // Edit用カメラ
+            overviewVCam.Priority = PriorityInactive;
+            buildVCam.Priority = PriorityActive;
+            Debug.Log("camerachange: buildVCam");
+        }
+        else // Playing, リザルト画面など
+        {
+            // 通常カメラに
+            overviewVCam.Priority = PriorityActive;
+            buildVCam.Priority = PriorityInactive;
+            Debug.Log("camerachange: overviewVCam");
+        }
+    }
+
+}
