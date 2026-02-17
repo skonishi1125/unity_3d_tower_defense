5dd1d966f17ff371fb60dbd0a31a0bbbafd1a601
skonishi <skonishi1125@gmail.com>
2026-02-17 23:31:30 +0900

    チュートリアルUIがボタンを押すことで表示されるようになった

---
+++ Assets/Script/UI/OpenTutorialButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class OpenTutorialButton : MonoBehaviour
+{
+    public static event Action OnOpenTutorialRequested;
+
+    public void ClickOpenTutorial()
+    {
+        Debug.Log("click tutorial");
+        OnOpenTutorialRequested?.Invoke();
+    }
+
+}
---
+++ Assets/Script/UI/Tutorial.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject wrapper;
+
+    private void Awake()
+    {
+        wrapper.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        OpenTutorialButton.OnOpenTutorialRequested += ActivePanel;
+    }
+
+    private void OnDisable()
+    {
+        OpenTutorialButton.OnOpenTutorialRequested -= ActivePanel;
+    }
+
+    private void ActivePanel()
+    {
+        Debug.Log("active des");
+
+        wrapper.SetActive(true);
+    }
+
+    // x ボタンに付与する関数
+    public void InactivePanel()
+    {
+        wrapper.SetActive(false);
+    }
+
+
+}
